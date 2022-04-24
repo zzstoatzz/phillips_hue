818c047de6550fcba5abdfc3de7291d683d06ea6
Nathan Nowack <thrast36@gmail.com>
2022-04-24 16:19:26 -0500

    add scene by group

--- flow.py
+++ flow.py
@@ -1,53 +1,58 @@
-from difflib import get_close_matches
 from phue import Bridge
 from prefect import task, flow
-from time import sleep
-from typing import Any, Union
 
-def connect() -> Bridge:
-    PHILLIPS_HUE_BRIDGE_IP = '192.168.0.165'
+modes = {
+    'main': {
+        'kitchen': 'Arctic aurora',
+        'office': 'Blood moon',
+        'living room': 'sahara',
+        'bedroom': 'Nightlight'
+    },
+    'clean': {
+        'kitchen': 'Bright',
+        'office': 'Bright',
+        'living room': 'Bright',
+        'bedroom': 'Bright'
+    },
+    'morning': {
+        'kitchen': 'Ocean dawn',
+        'office': 'Tropical twilight',
+        'living room': 'monet',
+        'bedroom': 'Relax'
+    }
+}
 
-    return Bridge(PHILLIPS_HUE_BRIDGE_IP)
-
-
-def handle(status: Any):
-    match status[0][0]:
-        case {'error': error}:
-            print(f"{error['type']} ERROR: {error['description']}")
-            match error['description']:
-                case 'Device is set to off':
-                    print('wow this is cool')
-
-@task(name="Toggle some lights")
-def toggle(room: str, brightness: Union[int, dict]) -> None:
-    bridge = connect()
-
-    room_names = {j['name']: i for i, j in bridge.get_group().items()}
-
-    room_number = room_names[get_close_matches(room, room_names.keys())[0]]
-
-    lights = bridge.get_group(int(room_number), 'lights')
-
-    for light_id in lights:
-        config = {
-            'bri': brightness
-        }
-
-        status = bridge.set_light(int(light_id), config)
-
-        handle(status)
-
-
-
-        
+@task
+def update_group_lights(
+    bridge: Bridge,
+    group_name: str,
+    scene_name: str,
+    transition: int = 100 # 100 * 0.1 second
+) -> bool:
+            
+    print(f'Setting {group_name} to {scene_name}')
+    
+    return bridge.run_scene(
+        group_name=group_name,
+        scene_name=scene_name,
+        transition_time=transition
+    )
 
 @flow
-def smart_home():
+def smart_home(choice: str) -> None:
+        
+    bridge = Bridge()
 
-    room = 'kitchen'
-    brightness = 150
+    mode = modes[choice]
 
-    toggle(room, brightness)
+    for group_name, scene_name in mode.items():
+        try:
+            assert update_group_lights(bridge, group_name, scene_name)
+        
+        except AssertionError:
+            print(f'Failed to run scene in {group_name}')
+            raise
 
 if __name__ == "__main__":
-    smart_home()
+    smart_home('main')
+    
